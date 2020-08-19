85ba0344dc3fff7d26200c08f714c42b1ee23561
Beasleydotcom <beasley.com@gmail.com>
2020-08-19 13:02:08 -0700

    this is the one I CAN'T FIGURE OUT! YET..

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 'use strict'
+// const mongoose = require('mongoose');
 const server = require('./lib/server.js');
 require('dotenv');
+// const MONGODB_URI = 'mongodb://localhost:27017/class-08';
+
+// const mongooseOptions = {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// };
+
+// mongoose.connect(MONGODB_URI, mongooseOptions);
 server.start();
