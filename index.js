dc6b9ffdc132f9614fe8cced0e38b34986ddd5d4
Beasleydotcom <beasley.com@gmail.com>
2020-08-20 16:34:14 -0700

    stuff for joe

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 
 const mongooseOptions = {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    useUnifiedTopology: true,
 };
 
-mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
-server.start(process.env.PORT);
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions, () =>{
+    console.log('connected to mongodb')
+})
+server.start(3005);
